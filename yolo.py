9a8a4926d1836ac6265f9002046ade190c909be3
Ethan Causa <39714370+ECausa@users.noreply.github.com>
2025-10-30 09:01:22 -0500

    Add files via upload

---
+++ yolo.py
@@ -0,0 +1,53 @@
+from ultralytics import YOLO
+import cv2
+
+# Load YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+# Open camera (1 = default webcam for Sams MacBook)
+cap = cv2.VideoCapture(0)
+
+# load lables to filter results
+filterLables = []
+with open('labels.txt','r') as f:
+    content = f.read()
+    filterLables = content.splitlines()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Run YOLO detection
+    results = model(frame, stream=True)  # stream=True yields results in real-time
+
+    # Loop through results and draw boxes
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+            cls = int(box.cls[0])
+            conf = box.conf[0]
+            label = r.names[cls]
+            
+            # check for macthing items
+            if label in filterLables:
+               if "apple" in label:
+                   print("apple price $0.75/lb")
+               if "banana" in label:
+                   print("banana price $0.54/lb")
+               if "orange" in label:
+                   print("orange price $1.33/lb")
+
+
+
+            # Draw rectangle + label
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+            cv2.putText(frame, f"{label} {conf:.2f}", (int(x1), int(y1) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
+
+    cv2.imshow("YOLO Live Detection", frame)
+    if cv2.waitKey(1) == 27:  # ESC key to stop
+        break
+
+cap.release()
+cv2.destroyAllWindows()
